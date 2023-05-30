f1771cd389c702e850d23f2a58a610b3ab4aba3d
Augeva34 <133910385+Augeva34@users.noreply.github.com>
2023-05-30 17:23:33 +0000

    pao

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function alerta (){
     alert ("piroca")
+    const usuario = document.getElementsByTagName("input")[0].value
+    const senha = document.getElementsByTagName("input")[1].value
+    alert ("meu usuario é" + usuario)
+    alert ("minha senha é" + senha )
+
+    if (usuario == "augeva" && senha == "12345678"){
+    
+        alert ("bem vindo")
+    }else{
+        alert ("login não autorizado! cai fora!")
+    }
 }
